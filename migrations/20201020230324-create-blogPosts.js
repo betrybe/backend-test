1db48408505ec83e736add47464280405944117c
Marco Barbosa <marco.meireles.b@gmail.com>
2020-10-20 20:31:29 -0300

    Migations criadas

---
+++ migrations/20201020230324-create-blogPosts.js
@@ -0,0 +1,43 @@
+// 'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const PostsTable = queryInterface.createTable('Posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+    return PostsTable;
+  },
+
+  down: async (queryInterface) => queryInterface.dropTable('Posts'),
+};
