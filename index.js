2d33e585c22a6d0a9326c6c6775d10d54e3e8762
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-10-20 15:11:04 -0300

    Partiu fast

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.listen(3000, () => console.log('ouvindo porta 3000!!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
