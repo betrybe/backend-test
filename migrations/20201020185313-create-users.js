eaeee12f66da72ebcf7c6ca69cc3c07c4dda88d1
fabemiliano <fab.emiliano@gmail.com>
2020-10-20 21:43:19 -0300

    first requisite

--- migrations/20201020185313-create-users.js
+++ migrations/20201020185313-create-users.js
@@ -1,8 +1,6 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const UsersTable = queryInterface.createTable("Users", {
+    const UsersTable = queryInterface.createTable('Users', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -29,6 +27,6 @@ module.exports = {
     });
     return UsersTable;
   },
-  down: async (queryInterface) => queryInterface.dropTable("Users"),
+  down: async (queryInterface) => queryInterface.dropTable('Users'),
 
 };
