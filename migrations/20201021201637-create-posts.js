3a658dc93564eb7744fc0e33564ff1e3dc4cf31e
mirellaalves <mirella.alves@gmail.com>
2020-10-21 17:28:32 -0300

    create posts migration

---
+++ migrations/20201021201637-create-posts.js
@@ -0,0 +1,39 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Posts', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: { model: 'Users', key: 'id' },
+      },
+      published: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      update: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('Posts');
+  },
+};
