5d4a3d881dbd51818cf39cc9402b992c7ab3363c
mirellaalves <mirella.alves@gmail.com>
2020-10-21 19:33:05 -0300

    create user model

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const createUser = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  });
+
+  return User;
+};
+
+module.exports = createUser;
