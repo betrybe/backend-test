901080352a135bd9418ea9c66a9ecafa5a38c3cd
fabemiliano <fab.emiliano@gmail.com>
2020-10-22 17:15:23 -0300

    teste

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-<<<<<<< HEAD
 const bodyParser = require('body-parser');
 const userController = require('./controllers/userController');
 const postController = require('./controllers/postController');
@@ -16,13 +15,6 @@ app.delete('/user/me', auth, userController.deleteSelfUser);
 app.post('/post', auth, postController.createPost);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
-=======
-
-const app = express();
-
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
-
->>>>>>> 3ce1eebbb7db1be112b894350b8ace3c88f4e9d6
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
