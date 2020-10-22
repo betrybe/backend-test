e3c0b03ca2848a28aa492e7d21b31d79e4023ee3
Marco Barbosa <marco.meireles.b@gmail.com>
2020-10-22 19:13:20 -0300

    Requisito 9 passando nos testes locais

--- controllers/postController.js
+++ controllers/postController.js
@@ -42,6 +42,49 @@ postActions.get('/:id', authMiddleware, async (req, res) => {
     });
 });
 
+postActions.put('/:id', authMiddleware, async (req, res) => {
+  const { id } = req.params;
+  const { email } = req.user;
+  const { title, content } = req.body;
+  // Verifica itens obrigatórios
+  if (!title) {
+    return res.status(400).send({ message: '"title" is required' });
+  }
+  if (!content) {
+    return res.status(400).send({ message: '"content" is required' });
+  }
+
+  // Recebe os dados do post e do usuário logado
+  const post = await Post.findOne({ where: { id } });
+  const user = await User.findOne({ where: { email } });
+  if (!post) {
+    return res.status(404).send({ message: 'Post não existe' });
+  }
+  if (!user) {
+    return res.status(404).send({ message: 'Usuário não existe' });
+  }
+
+  // Salva os respectivos ids em constantes
+  const postUserId = post.dataValues.userId;
+  const loggedUserId = user.dataValues.id;
+
+  // Se os ids forem diferentes
+  if (postUserId !== loggedUserId) {
+    return res.status(401).send({ message: 'Usuário não autorizado' });
+  }
+
+  // Se estiver tudo certo atualiza o post
+  Post.update(
+    { title, content },
+    { where: { id } },
+  )
+    .then(() => res.status(200).send({ title, content, userId: loggedUserId }))
+    .catch((e) => {
+      console.log(e.message);
+      return res.status(500).send({ message: 'Algo deu errado' });
+    });
+});
+
 module.exports = {
   postActions,
 };
