d6b19e2cb955528662cf0297bb89615cc0207fcc
Gustaff <gustavohmfigueiredo@gmail.com>
2020-10-22 16:24:40 -0300

    no serach

--- controllers/post.js
+++ controllers/post.js
@@ -60,11 +60,11 @@ const deletePost = rescue(async (req, res) => {
   return res.status(204).end();
 });
 
-const search = rescue(async () => {
-  const { q } = req.query;
+// const search = rescue(async () => {
+//   const { q } = req.query;
 
-  if ()
-});
+//   if ()
+// });
 
 module.exports = {
   createPost,
@@ -73,5 +73,5 @@ module.exports = {
   updatePost,
   deletePost,
   userOwnerShip,
-  search,
+  // search,
 };
