9a6d94d903227a97bea8f713bdfb9209eee22ca9
Marco Barbosa <marco.meireles.b@gmail.com>
2020-10-22 00:20:49 -0300

    Adicionando o dotenv no index

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Primeiro commit
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const user = require('./controllers/userController');
