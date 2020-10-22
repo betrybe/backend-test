698ab4cf6a1370277b2b512c79401c20ebcefb2a
Rodrigo Militao <militaorodrigo9@gmail.com>
2020-10-22 17:10:30 -0300

    delete /me working

--- routers/user.js
+++ routers/user.js
@@ -7,6 +7,7 @@ const usersRouter = express.Router();
 usersRouter
   .get('/', middlewares.auth(), usersControllers.getAll)
   .get('/:id', middlewares.auth(), usersControllers.getUserById)
-  .post('/', usersControllers.register);
+  .post('/', usersControllers.register)
+  .delete('/me', middlewares.auth(), usersControllers.deleteUser);
 
 module.exports = usersRouter;
