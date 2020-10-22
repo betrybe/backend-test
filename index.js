647b73d7b93f852ec2fcb9351aa86e85f102ba56
Marco Barbosa <marco.meireles.b@gmail.com>
2020-10-22 10:11:37 -0300

    Ajeitando o index do models

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Primeiro commit
-require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const user = require('./controllers/userController');
