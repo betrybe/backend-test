049bc22ef3a2c304ba5f4e06f731c315f5dd3ae9
Ilan Herbach <ilan.herbach@gmail.com>
2020-10-23 07:17:52 -0300

    Removed unused try..catch block

--- utils/validateToken.js
+++ utils/validateToken.js
@@ -2,22 +2,18 @@ const jwt = require('jsonwebtoken');
 const SECRET = require('./SECRET');
 
 const validateToken = (req, res, next) => {
-  try {
-    const token = req.headers.authorization;
-    if (!token) {
-      return res.status(401).json({ message: 'Token não encontrado' });
-    }
-    jwt.verify(token, SECRET, (err, userData) => {
-      if (err) {
-        return res.status(401).json({ message: 'Token expirado ou inválido' });
-      }
-      req.token = token;
-      req.user = userData;
-      next();
-    });
-  } catch (error) {
+  const token = req.headers.authorization;
+  if (!token) {
     return res.status(401).json({ message: 'Token não encontrado' });
   }
+  jwt.verify(token, SECRET, (err, userData) => {
+    if (err) {
+      return res.status(401).json({ message: 'Token expirado ou inválido' });
+    }
+    req.token = token;
+    req.user = userData;
+    next();
+  });
 };
 
 module.exports = validateToken;
