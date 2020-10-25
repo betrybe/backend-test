9db7ed3912680bd14698b042234440a808bbc0c9
Ilan Herbach <ilan.herbach@gmail.com>
2020-10-25 20:45:17 -0300

    'where' clause changed while searching for a specific user using Sequelize

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -5,17 +5,13 @@ const createToken = require('../utils/createToken');
 const loginController = async (req, res) => {
   const { email, password } = req.body;
   const searchUser = await User.findOne({
-    where: {
-      [Op.and]: [
-        { email: { [Op.eq]: email } },
-        { password: { [Op.eq]: password } },
-      ],
-    },
+    where: { email, password },
   });
   if (searchUser === null) {
     res.status(400).json({ message: 'Campos inválidos' });
   } else {
-    const token = createToken(email);
+    const { id } = searchUser;
+    const token = createToken({ id, email });
     res.json({ token });
   }
 };
