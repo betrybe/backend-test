3f3f5e76fcf834d9fa1e952cc6e81a9d6cbc22cf
Clayton Pereira <cpereiramt@gmail.com>
2020-10-24 21:43:23 -0400

    configurando sequelize

---
+++ migrations/20201025010735-create-users.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+  const UsersTable = queryInterface.createTable('Users',{
+  id: {
+    autoIncrement: true,
+    primaryKey: true,
+    type: Sequelize.INTEGER,
+  }, 
+  displayName: {
+    type: Sequelize.STRING,
+  },
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+  },
+  password: {
+    type: Sequelize.INTEGER,
+  },
+  image: {
+    type: Sequelize.STRING,
+  }
+  })
+  return UsersTable;
+  },
+
+  down: async (queryInterface) => {
+  queryInterface.dropTable('Users');
+  }
+};
---
+++ migrations/20201025010813-create-posts.js
@@ -0,0 +1,37 @@
+
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const PostsTable = queryInterface.createTable('Posts',{
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      content: {
+        type: Sequelize.STRING,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: { model: 'Users', key: 'id' },
+      },
+      published: {
+        type: Sequelize.DATE,
+      },
+      updated: {
+        type: Sequelize.DATE,
+      }
+      })
+      return PostsTable;
+  },
+
+  down: async (queryInterface) => {
+     queryInterface.dropTable('Posts');
+  }
+};
