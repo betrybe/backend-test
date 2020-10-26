d8cae1e594879585203332d7713e228892b9ffa8
Clayton Pereira <cpereiramt@gmail.com>
2020-10-26 08:43:36 -0400

    Salvando alteraçoes no requisito 1

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 require('dotenv/config');
 
-const secret = process.env.SECRET;
+const secret = process.env.SECRET || 'jwtSecret';
 
 const authClient = () => (req, res, next) => {
   const { body } = req;
