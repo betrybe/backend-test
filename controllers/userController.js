fbe23f89dbda00fd3325eea1b11eae124086d279
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-10-26 18:00:06 -0300

    Tentando resolver problemas de headers.

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,30 +7,46 @@ const user = Router();
 
 user.post(
   '/',
-  rescue(async (req, res) => {
+  rescue(async (req, res, next) => {
     const { displayName, email, password, image } = req.body;
     const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
-    if (displayName.length < 8) { res.status(400).json({ message: '"displayName" length must be at least 8 characters long' }); }
+    if (displayName.length < 8) {
+      return res.status(400).json({
+        message: '"displayName" length must be at least 8 characters long',
+      });
+    }
 
-    if (email === undefined) res.status(400).json({ message: '"email" is required' });
+    if (!email) {
+      return res.status(400).json({ message: '"email" is required' });
+    }
 
-    if (!email.match(emailRegex)) { res.status(400).json({ message: '"email" must be a valid email' }); }
+    if (password) res.status(400).json({ message: '"password" is required' });
 
-    if (password.length < 6) { res.status(400).json({ message: '"password" length must be at least 6 characters long' }); }
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: '"email" must be a valid email' });
+    }
 
-    if (password === undefined) res.status(400).json({ message: '"password" is required' });
+    if (!password) {
+      return res.status(400).json({ message: '"password" is required' });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: '"password" length must be 6 characters long' });
+    }
 
     try {
       const userInfo = await Users.create({ displayName, email, password, image });
       const { password: senha, ...data } = userInfo.dataValues;
       const token = generateJWT(data);
-
       return res.status(201).json({ token });
     } catch (error) {
       console.log(error.message);
       if (error.message === 'Validation error') res.status(409).json({ message: 'Usuário já existe' });
     }
+    next();
   }),
 );
 
