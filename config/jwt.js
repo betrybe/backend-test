f60c9064d9b808973347175835182cd77561ef3b
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2020-10-27 14:28:53 -0300

    requisito 1

--- config/jwt.js
+++ config/jwt.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 require('dotenv/config');
 
-const SECRET = process.env.SECRET;
+const { SECRET } = process.env.SECRET;
 const jwtConfig = { algorithm: 'HS256' };
 const GenerateToken = (userdata) => {
   const token = jwt.sign(userdata, SECRET, jwtConfig);
