8b157f38a72530f15334c250ceda05090f2a0fac
Clayton Pereira <cpereiramt@gmail.com>
2020-10-27 14:20:46 -0400

    Salvando alteraçoes requisito 6

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -4,11 +4,18 @@ const auth = require('../middleware/auth');
 
 const post = Router();
 
-post.post('/', auth, (req, res) => {
+post.post('/', auth, async (req, res) => {
   const { title, content } = req.body;
+  const { id } = req.user;
   console.log(req.user);
-  Posts.create({ title, content });
-  res.status(201).send('post');
+  if (title === undefined) {
+    return res.status(400).send({ message: '"title" is required' });
+  }
+  if (content === undefined) {
+    return res.status(400).send({ message: '"content" is required' });
+  }
+  const postCreated = await Posts.create({ title, content, userId: id });
+  return res.status(201).send(postCreated);
 });
 
 module.exports = post;
