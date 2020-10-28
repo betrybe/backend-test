b3b07f9df289fbd7586e5cc60d175683cec14ec9
wendersonbelko <contato@wendersonbelko.com>
2020-10-27 21:01:04 -0300

    test

--- config/config.js
+++ config/config.js
@@ -4,14 +4,14 @@ module.exports = {
   development: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.DB_NAME || 'blogs_api',
+    database: process.env.DB_NAME,
     host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
   test: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.DB_NAME || 'blogs_api',
+    database: process.env.DB_NAME,
     host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
