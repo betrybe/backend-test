3a39fc1311637d1d605802e0980ffa48819186de
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-10-28 16:41:19 -0300

    Requisito 7

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 // const rescue = require('express-rescue');
 const { validateJWT } = require('../middlewares/auth');
-const { Posts } = require('../models');
+const { Posts, Users } = require('../models');
 
 const post = Router();
 
@@ -21,6 +21,16 @@ post.post('/', validateJWT, async (req, res) => {
   } catch (error) {
     console.log(error.message);
   }
+});
+
+post.get('/', validateJWT, (_req, res) => {
+  Posts.findAll({ attributes: { exclude: ['userId'] },
+    include: {
+      model: Users,
+      as: 'user',
+      attributes: { exclude: ['password'] },
+    },
+  }).then((results) => res.status(200).json(results));
 });
 
 module.exports = post;
