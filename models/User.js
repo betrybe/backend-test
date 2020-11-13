7ac947e8f99801013bf685ade6feea4e08cc07ca
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-11-13 10:16:20 -0300

    indentation

--- models/User.js
+++ models/User.js
@@ -1,55 +1,55 @@
 const createUser = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-      id: {
+  const User = sequelize.define('User', {
+    id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
       allowNull: false,
     },
-      displayName: {
-        type: DataTypes.STRING,
-        validate: {
-          len: {
-            min: 8,
-            msg: '"displayName" length must be at least 8 characters long',
-          },
-        },
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        unique: {
-          msg: 'Usuário já existe',
-        },
-        allowNull: false,
-        validate: {
-          isEmail: {
-            msg: '"email" must be a valid email',
-          },
-          notNull: {
-            msg: '"email" is required',
-          },
+    displayName: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          min: 8,
+          msg: '"displayName" length must be at least 8 characters long',
         },
       },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: {
-            min: 6,
-            msg: '"password" length must be 6 characters long',
-          },
-          notNull: {
-            msg: '"password" is required',
-          },
+    	allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: {
+        msg: 'Usuário já existe',
+      },
+      allowNull: false,
+      validate: {
+        isEmail: {
+          msg: '"email" must be a valid email',
+        },
+        notNull: {
+          msg: '"email" is required',
         },
       },
-      image: {
-        type: DataTypes.STRING,
-        allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          min: 6,
+          msg: '"password" length must be 6 characters long',
+        },
+        notNull: {
+          msg: '"password" is required',
+        },
       },
-    }, { timestamps: false });
-    return User;
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, { timestamps: false });
+  return User;
 };
 
 module.exports = createUser;
