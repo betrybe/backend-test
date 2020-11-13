3e611b925c81f989f2fbff92eabf4b8088a7f486
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-11-13 16:47:03 -0300

    1 requisito passou localmente

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -9,20 +9,36 @@ user.post('/',
     try {
       console.log(req.body);
       const { email, password } = req.body;
+      if (email === undefined) {
+        res.status(400).json({ message: '"email" is required' });
+        throw new Error();
+      }
+      if (email === '') {
+        res.status(400).json({ message: '"email" is not allowed to be empty' });
+        throw new Error();
+      }
+      if (password === undefined) {
+        res.status(400).json({ message: '"password" is required' });
+        throw new Error();
+      }
+      if (password === '') {
+        res.status(400).json({ message: '"password" is not allowed to be empty' });
+        throw new Error();
+      }  
       const userLog = await User.findOne({
         where: { email, password },
       });
       if (!userLog) {
         res.status(400).json({ message: 'Campos inválidos' });
-      }
-      else {
-        res.json(createToken(userLog.id, email))
+      } else {
+        res.json(createToken(userLog.id, email));
       }
     } catch (error) {
       console.error('erro aqui', error);
       return next(error);
     }
   },
+
 );
 
 module.exports = user;
