f179c3332f3f56dccdae188399bdc0a3a3c345a4
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-11-13 04:42:39 -0300

    setado model

---
+++ migrations/20201113042019-create-blog-posts.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const BlogPostsTable = queryInterface.createTable('Posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+
+    return BlogPostsTable;
+  },
+
+  down: async (queryInterface) => queryInterface.dropTable('Posts'),
+};
