be4e708ed172fadf65b453bb7857995700dcd987
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-11-13 10:34:19 -0300

    spaces not tabs

--- models/Post.js
+++ models/Post.js
@@ -1,40 +1,38 @@
 const createPost = (sequelize, DataTypes) => {
-	const Post = sequelize.define('Post', {
-	id: {
-	type: DataTypes.INTEGER,
-	primaryKey: true,
-	autoIncrement: true,
-	allowNull: false,
-	},
-	title: {
-	type: DataTypes.STRING,
-			allowNull: false,
-			validate: {
-			notNull: {
-					msg: '"title" is required',
-			},
-			},
-	},
-	content: {
-		type: DataTypes.STRING,
-		allowNull: false,
-		validate: {
-			notNull: {
-			msg: '"content" is required',
-			},
-			},
-	},
-	userId: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			foreignKey: true,
-		},
-	}, { timestamps: true, updatedAt: 'updated', createdAt: 'published' });
+  const Post = sequelize.define('Post', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: '"title" is required',
+        },
+      },
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        msg: '"content" is required',
+      },
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      foreignKey: true,
+    },
+  }, { timestamps: true, updatedAt: 'updated', createdAt: 'published' });
 
-	Post.associate = (model) => {
-		Post.belongsTo(model.User, { as: 'user', foreignKey: 'userId' });
-	};
-	return Post;
+  Post.associate = (model) => {
+    Post.belongsTo(model.User, { as: 'user', foreignKey: 'userId' });
+  };
+  return Post;
 };
 
 module.exports = createPost;
