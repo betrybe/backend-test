7a29c94d8b5cf8b8c37131eadcc8932554fd4efb
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-11-13 06:02:26 -0300

    lintered

--- config/config.js
+++ config/config.js
@@ -1,25 +1,25 @@
 require('dotenv').config();
 
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": process.env.PASSWORD,
-    "database": "blogs_api",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  development: {
+    username: 'root',
+    password: process.env.PASSWORD,
+    database: 'blogs_api',
+    host: '127.0.0.1',
+    dialect: 'mysql'
   },
-  "test": {
-    "username": "root",
-    "password": process.env.PASSWORD,
-    "database": "blogs_api",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: 'root',
+    password: process.env.PASSWORD,
+    database: 'blogs_api',
+    host: '127.0.0.1',
+    dialect: 'mysql'
   },
-  "production": {
-    "username": "root",
-    "password": process.env.PASSWORD,
-    "database": "blogs_api",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
+  production: {
+    username: 'root',
+    password: process.env.PASSWORD,
+    database: 'blogs_api',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  },
 };
