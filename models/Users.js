80c3f6b04e232d65e711036a613253909a2e29a0
gabryelryba <gabryel.ryba@gmail.com>
2020-11-15 19:22:47 -0300

    removed validations from sequelize models

--- models/Users.js
+++ models/Users.js
@@ -3,9 +3,9 @@ const Users = (sequelize, DataTypes) => {
     'User',
     {
       id: { type: DataTypes.INTEGER, primaryKey: true, allowNull: false, autoIncrement: true },
-      displayName: { type: DataTypes.STRING, allowNull: false, validate: { isEmail: true } },
+      displayName: { type: DataTypes.STRING, allowNull: false },
       email: { type: DataTypes.STRING, allowNull: false, unique: true },
-      password: { type: DataTypes.STRING, allowNull: false, validate: { len: [6, 6] } },
+      password: { type: DataTypes.STRING, allowNull: false },
       image: { type: DataTypes.STRING, allowNull: true },
     },
     {
