173086293a1abcc6b9e2dfd293dbd6fdc2ec233c
gabryelryba <gabryel.ryba@gmail.com>
2020-11-15 22:57:13 -0300

    fixed defaultValues timestamps at posts model

--- migrations/20201021210233-create-posts-table.js
+++ migrations/20201021210233-create-posts-table.js
@@ -1,36 +1,36 @@
 module.exports = {
-  up: async (queryInterface, Sequelize) => queryInterface.createTable('Posts', {
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
+  up: async (queryInterface, Sequelize) =>
+    queryInterface.createTable('Posts', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      published: {
+        type: Sequelize.DATE,
+      },
+      updated: {
+        type: Sequelize.DATE,
+      },
+    }),
 
-    },
-    title: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    content: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    userId: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      references: { model: { tableName: 'Users' }, key: 'id' },
-      onUpdate: 'CASCADE',
-      onDelete: 'CASCADE',
-    },
-    published: {
-      type: Sequelize.DATE,
-      allowNull: false,
-    },
-    updated: {
-      type: Sequelize.DATE,
-      allowNull: false,
-    },
-  }),
-
-  down: async (queryInterface, _Sequelize) => queryInterface.dropTable('Users'),
+  down: async (queryInterface) => queryInterface.dropTable('Posts'),
 };
