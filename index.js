8ff1980b44255ce8b26bac81bfc06e313555e724
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-11-17 22:15:08 -0300

    ver se vai

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!!`));
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
+app.get('/', (request, response) => {
   response.send();
 });
 app.use('/login', controllers.loginController);
