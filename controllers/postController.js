6582cddaef3d3b871ceeb49fbdb8206fb3925076
mirellaalves <mirella.alves@gmail.com>
2020-11-20 00:29:35 -0300

    try to correct blog post and start get posts, get post by id and delete post

--- controllers/postController.js
+++ controllers/postController.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 const createPost = async (req, res) => {
   const { title, content } = req.body;
-  const { userId } = req.user;
+  const { dataValues: { id: userId } } = req.user;
 
   const validation = await validatePostData(title, content);
   if (validation.error) {
@@ -17,12 +17,41 @@ const createPost = async (req, res) => {
 
   const post = await Post.create({ title, content, userId });
   console.log('post: ', post);
-  res.status(201).json(post);
+  res.status(201).json(post.dataValues);
+};
+
+const getPosts = async (_req, res) => {
+  const posts = await Post.findAll({});
+  res.status(200).json(posts);
+};
+
+const getPostById = async (req, res) => {
+  const post = await Post.findByPk(req.params.id);
+  if (!post) {
+    return res.status(404).json({ message: 'Post não existe' });
+  }
+  res.status(200).json(post);
+};
+
+const deletePostById = async (req, res) => {
+  const { id } = req.params;
+  const post = await Post.findByPk(id);
+  if (!post) {
+    return res.status(404).json({ message: 'Post não existe' });
+  }
+
+  const { dataValues: { id: userId } } = req.user;
+  if (post.dataValues.userId !== userId) {
+    return res.status(401).json({ message: 'Usuário não autorizado' });
+  }
+
+  await Post.destroy({ where: { id } });
+  res.status(204).end();
 };
 
 router.post('/', authMiddleware, rescue(createPost));
-// router.get('/', authMiddleware, rescue(getUsers));
-// router.get('/:id', authMiddleware, rescue(getUserById));
-// router.delete('/me', authMiddleware, rescue(deleteUser));
+router.get('/', authMiddleware, rescue(getPosts));
+router.get('/:id', authMiddleware, rescue(getPostById));
+router.delete('/:id', authMiddleware, rescue(deletePostById));
 
 module.exports = router;
