9c5899d03d154ad73800d50942e25e3dde3c1538
Pedro Ivo <pedryvo@gmail.com>
2021-05-26 04:21:50 -0300

    fix user route path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create]
+  resources :users, only: [:create], path: 'user'
   post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
 end
