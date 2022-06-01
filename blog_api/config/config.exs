b7fedd6c8da2f74f5c94f6199e2905b0b05049b4
Carolina Alves dos Santos <xcarolina.alves@gmail.com>
2022-06-01 00:57:50 -0300

    feat: configurando Guardian

--- blog_api/config/config.exs
+++ blog_api/config/config.exs
@@ -50,3 +50,7 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+config :blog_api, BlogApiWeb.Auth.Guardian,
+  issuer: "blog_api",
+  secret_key: "/ek7fTUWtQ8zXFPPAAMkTSmNMKPiEFVfWaeX2rtZG+E5tUPSVDzCvebCoQRo0dCV"
---
+++ blog_api/lib/blog_api_web/auth/guardian.ex
@@ -0,0 +1,13 @@
+defmodule BlogApiWeb.Auth.Guardian do
+  use Guardian, otp_app: :blog_api
+
+  def subject_for_token(%{id: id}, _claims) do
+    sub = to_string(id)
+    {:ok, sub}
+  end
+
+  def resource_from_claims(%{"sub" => id}) do
+    id
+    |> BlogApi.fetch_user()
+  end
+end
